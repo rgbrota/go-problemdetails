3a9ab45a30ea002cf461f7dd0f426de4a632aec1
rgbrota <rgbrota@gmail.com>
2022-10-02 19:31:13 +0200

    Refactor tests

--- problemdetails_test.go
+++ problemdetails_test.go
@@ -25,18 +25,18 @@ func TestNew(t *testing.T) {
 
 	tests := []test{
 		{
-			name: "status_http_200_blank_type",
+			name: "status_http_500_blank_type",
 			args: args{
 				problemType: "",
 				title:       "",
-				statusCode:  200,
+				statusCode:  http.StatusInternalServerError,
 				detail:      "",
 				instance:    "",
 			},
 			expected: &ProblemDetails{
 				Type:     defaultProblemType,
-				Title:    http.StatusText(200),
-				Status:   200,
+				Title:    http.StatusText(http.StatusInternalServerError),
+				Status:   http.StatusInternalServerError,
 				Detail:   "",
 				Instance: "",
 			},
@@ -45,15 +45,15 @@ func TestNew(t *testing.T) {
 			name: "status_http_404",
 			args: args{
 				problemType: "https://some-domain.com/not_found",
-				title:       http.StatusText(404),
-				statusCode:  404,
+				title:       http.StatusText(http.StatusNotFound),
+				statusCode:  http.StatusNotFound,
 				detail:      "Object with id 1 was not found",
 				instance:    "https://api.some-domain.com/xxx/1",
 			},
 			expected: &ProblemDetails{
 				Type:     "https://some-domain.com/not_found",
-				Title:    http.StatusText(404),
-				Status:   404,
+				Title:    http.StatusText(http.StatusNotFound),
+				Status:   http.StatusNotFound,
 				Detail:   "Object with id 1 was not found",
 				Instance: "https://api.some-domain.com/xxx/1",
 			},
@@ -62,15 +62,15 @@ func TestNew(t *testing.T) {
 			name: "status_http_500_relative_uris",
 			args: args{
 				problemType: "/internal_server_error",
-				title:       http.StatusText(500),
-				statusCode:  500,
+				title:       http.StatusText(http.StatusInternalServerError),
+				statusCode:  http.StatusInternalServerError,
 				detail:      "There was an error",
 				instance:    "example-1",
 			},
 			expected: &ProblemDetails{
 				Type:     "/internal_server_error",
-				Title:    http.StatusText(500),
-				Status:   500,
+				Title:    http.StatusText(http.StatusInternalServerError),
+				Status:   http.StatusInternalServerError,
 				Detail:   "There was an error",
 				Instance: "example-1",
 			},
@@ -99,14 +99,14 @@ func TestFromHTTPStatus(t *testing.T) {
 
 	tests := []test{
 		{
-			name: "status_http_200",
+			name: "status_http_400",
 			args: args{
-				statusCode: 200,
+				statusCode: http.StatusBadRequest,
 			},
 			expected: &ProblemDetails{
 				Type:     defaultProblemType,
-				Title:    http.StatusText(200),
-				Status:   200,
+				Title:    http.StatusText(http.StatusBadRequest),
+				Status:   http.StatusBadRequest,
 				Detail:   "",
 				Instance: "",
 			},
@@ -114,12 +114,12 @@ func TestFromHTTPStatus(t *testing.T) {
 		{
 			name: "status_http_500",
 			args: args{
-				statusCode: 500,
+				statusCode: http.StatusInternalServerError,
 			},
 			expected: &ProblemDetails{
 				Type:     defaultProblemType,
-				Title:    http.StatusText(500),
-				Status:   500,
+				Title:    http.StatusText(http.StatusInternalServerError),
+				Status:   http.StatusInternalServerError,
 				Detail:   "",
 				Instance: "",
 			},
@@ -158,26 +158,26 @@ func TestJSONMarshal(t *testing.T) {
 
 	tests := []test{
 		{
-			name: "status_http_200_blank_type",
+			name: "status_http_500_blank_type",
 			args: &ProblemDetails{
 				Type:     defaultProblemType,
-				Title:    http.StatusText(200),
-				Status:   200,
+				Title:    http.StatusText(http.StatusInternalServerError),
+				Status:   http.StatusInternalServerError,
 				Detail:   "",
 				Instance: "",
 			},
-			expected: fmt.Sprintf(`{"type":"%s","title":"%s","status":200}`, defaultProblemType, http.StatusText(200)),
+			expected: fmt.Sprintf(`{"type":"%s","title":"%s","status":%v}`, defaultProblemType, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError),
 		},
 		{
 			name: "status_http_400_error",
 			args: &ProblemDetails{
 				Type:     "https://some-domain.com/validation-failed",
 				Title:    "Validation error",
-				Status:   400,
+				Status:   http.StatusBadRequest,
 				Detail:   "Your request parameters didn't validate",
 				Instance: "https://api.some-domain.com/example",
 			},
-			expected: `{"type":"https://some-domain.com/validation-failed","title":"Validation error","status":400,"detail":"Your request parameters didn't validate","instance":"https://api.some-domain.com/example"}`,
+			expected: fmt.Sprintf(`{"type":"https://some-domain.com/validation-failed","title":"Validation error","status":%v,"detail":"Your request parameters didn't validate","instance":"https://api.some-domain.com/example"}`, http.StatusBadRequest),
 		},
 	}
 
@@ -203,26 +203,26 @@ func TestXMLMarshal(t *testing.T) {
 
 	tests := []test{
 		{
-			name: "status_http_200_blank_type",
+			name: "status_http_500_blank_type",
 			args: &ProblemDetails{
 				Type:     defaultProblemType,
-				Title:    http.StatusText(200),
-				Status:   200,
+				Title:    http.StatusText(http.StatusInternalServerError),
+				Status:   http.StatusInternalServerError,
 				Detail:   "",
 				Instance: "",
 			},
-			expected: fmt.Sprintf(`<problem xmlns="urn:ietf:rfc:7807"><type>%s</type><title>%s</title><status>200</status></problem>`, defaultProblemType, http.StatusText(200)),
+			expected: fmt.Sprintf(`<problem xmlns="urn:ietf:rfc:7807"><type>%s</type><title>%s</title><status>%v</status></problem>`, defaultProblemType, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError),
 		},
 		{
 			name: "status_http_400_error",
 			args: &ProblemDetails{
 				Type:     "https://some-domain.com/validation-failed",
 				Title:    "Validation error",
-				Status:   400,
+				Status:   http.StatusBadRequest,
 				Detail:   "Your request parameters didn't validate",
 				Instance: "https://api.some-domain.com/example",
 			},
-			expected: `<problem xmlns="urn:ietf:rfc:7807"><type>https://some-domain.com/validation-failed</type><title>Validation error</title><status>400</status><detail>Your request parameters didn&#39;t validate</detail><instance>https://api.some-domain.com/example</instance></problem>`,
+			expected: fmt.Sprintf(`<problem xmlns="urn:ietf:rfc:7807"><type>https://some-domain.com/validation-failed</type><title>Validation error</title><status>%v</status><detail>Your request parameters didn&#39;t validate</detail><instance>https://api.some-domain.com/example</instance></problem>`, http.StatusBadRequest),
 		},
 	}
 
